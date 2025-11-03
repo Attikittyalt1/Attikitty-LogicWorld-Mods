4df02fd063c01078811ec77d233bad6ebc162f09
Attikittyalt1 <103009142+Attikittyalt1@users.noreply.github.com>
2025-11-03 11:30:26 -0600

    Added BoardPegs, again...

---
+++ BoardPegs/src/server/BoardPegTop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BoardPegs.Logic;
+
+public class BoardPegTop : BoardPeg
+{
+    public override bool ShouldBeLinkedHorizontally()
+    {
+        return Mathf.Abs(Component.localUp.y) >= MinimumValue;
+    }
+
+    public override bool ShouldBeLinkedVertically()
+    {
+        return Mathf.Abs(Component.localUp.y) >= MinimumValue;
+    }
+}
