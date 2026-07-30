164479b43b43a091b081621125bab59bbbda7c27
Attikittyalt1 <103009142+Attikittyalt1@users.noreply.github.com>
2026-07-30 03:03:30 -0500

    added shifter. still currently bugged so that only one carry is connected

---
+++ CondensedCablingBasicLogicComponents/CondensedCablingBasicLogicComponents/src/server/components/SuperShifter.cs
@@ -0,0 +1,45 @@
+﻿using LICC;
+using SkysCondensedCablingLib.Server;
+using System;
+using System.Linq;
+
+namespace CondensedCablingBasicLogicComponents.Server;
+
+public class SuperShifter : GenericSuperGate
+{
+    public override int InputSuperSize(int index) => index > 1 ? 0 : base.InputSuperSize(index);
+
+    public override bool InputAtIndexShouldTriggerComponentLogicUpdates(int inputIndex) => false;
+
+    protected override void OnCustomDataUpdated()
+    {
+        base.OnCustomDataUpdated();
+
+        CreateLinks();
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        CreateLinks();
+    }
+
+    private void CreateLinks()
+    {
+        var dataA = Inputs[0] as SuperInputPeg;
+        var dataB = Inputs[1] as SuperInputPeg;
+        var carryA = Inputs[2];
+        var carryB = Inputs[3];
+
+        var size = Math.Min(dataA.BaseSize, dataB.BaseSize);
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            dataA.AddPhasicLinkWith(dataB, (i, i+1));
+        }
+
+        LConsole.WriteLine(size);
+        //dataA.AddPhasicLinkWith(carryB, size - 1);
+        dataB.AddPhasicLinkWith(carryA, 0);
+    }
+}
